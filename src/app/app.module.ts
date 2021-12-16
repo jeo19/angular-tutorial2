0d7744cc19740bec4110a1c1105a2fdb73a2a3d9
jeo19 <terrakkk@outlook.com>
2021-12-16 09:39:00 +0800

    add:Add the routing moudle and feature components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatTableModule } from '@angular/material/table'
+import { MatTableModule } from '@angular/material/table';
+import { HomeComponent } from './pages/home/home.component';
+import { BookSearchMainComponent } from './bookSearch/book-search-main/book-search-main.component';
+import { MovieSearchMainComponent } from './movieSearch/movie-search-main/movie-search-main.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BookSearchMainComponent,
+    MovieSearchMainComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/bookSearch/book-search-main/book-search-main.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BookSearchMainComponent } from './book-search-main.component';
+
+describe('BookSearchMainComponent', () => {
+  let component: BookSearchMainComponent;
+  let fixture: ComponentFixture<BookSearchMainComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BookSearchMainComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookSearchMainComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

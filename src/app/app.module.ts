e0e37eb9552cea4c302b922f35546cd3a841d4d6
jeo19 <terrakkk@outlook.com>
2021-12-20 09:14:03 +0800

    feat:Add the detail-box and list-box template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { MovieSearchMainComponent } from './movieSearch/movie-search-main/movie-
 import { BookSearchModule } from './book-search/book-search.module';
 import { MovieSearchModule } from './movie-search/movie-search.module';
 import { SearchBoxComponent } from './bookSearch/search-box/search-box.component';
+import { DetailBoxComponent } from './bookSearch/detail-box/detail-box.component';
+import { ListBoxComponent } from './bookSearch/list-box/list-box.component';
 
 
 @NgModule({
@@ -19,7 +21,9 @@ import { SearchBoxComponent } from './bookSearch/search-box/search-box.component
     HomeComponent,
     BookSearchMainComponent,
     MovieSearchMainComponent,
-    SearchBoxComponent
+    SearchBoxComponent,
+    DetailBoxComponent,
+    ListBoxComponent
   ],
   imports: [
     BrowserModule,

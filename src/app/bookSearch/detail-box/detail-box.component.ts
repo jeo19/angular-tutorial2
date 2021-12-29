c9fda3301ce21e0913b8eb578a8f50d23578ef5e
jeo19 <terrakkk@outlook.com>
2021-12-29 09:04:45 +0800

    feat:Add the disable property binding

--- src/app/bookSearch/detail-box/detail-box.component.ts
+++ src/app/bookSearch/detail-box/detail-box.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailBoxComponent implements OnInit {
 
+  book={
+    btitle:'Head First Design Patterns:Deep Dive Angular',
+    bauthor:'Frymon and 3 other',
+    bprice:28000,
+    bdate:'2009/12/3',
+    bisbn:'89-7914-340-0',
+    bimgurl:'http://image.hanbit.co.kr/cover/_m_1340m.gif'
+  }
+
   constructor() { }
 
   ngOnInit(): void {
